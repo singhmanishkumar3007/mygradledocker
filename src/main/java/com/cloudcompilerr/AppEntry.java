4bc1dfd7fd0d2c5febf5cdb3b9dae7b44265fd9c
Lovely Singh <lovely@Lovelys-MacBook-Pro.local>
2018-05-26 17:26:25 +0530

    First commit using gradle.

---
+++ src/main/java/com/cloudcompilerr/AppEntry.java
@@ -0,0 +1,14 @@
+package com.cloudcompilerr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEntry {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEntry.class, args);
+
+	}
+
+}
